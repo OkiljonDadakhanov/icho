ebdd9c734fc872418d326543daa212a37ed45b9f
Okiljon Dadakhonov <okiljondadakhonov@gmail.com>
2025-01-07 06:30:37 +0500

    upd: icho

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true, // Optional: Use SWC for minification
-  basePath: '/<your-repository-name>', // replace with your repository name
-  assetPrefix: '/<your-repository-name>', // needed for asset files like images
+  basePath: '/icho', // replace with your repository name
+  assetPrefix: '/icho', // needed for asset files like images
   trailingSlash: true,
 };
 
