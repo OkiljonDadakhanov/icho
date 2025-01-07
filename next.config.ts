833c7aeed202f4fc8b09cc76698871f61f5096ec
Akilhan <oqiljondadaxanov@gmail.com>
2025-01-07 07:21:23 +0500

    last

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // Optional: Use SWC for minification
-  basePath: "/icho",
-  assetPrefix: "/icho",
 };
 
 export default nextConfig;
