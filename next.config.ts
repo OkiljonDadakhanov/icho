50edf4a3d681913e828829cc9570553d5ecfff86
Okiljon Dadakhonov <okiljondadakhonov@gmail.com>
2025-01-07 06:29:49 +0500

    fix: github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true, // Optional: Use SWC for minification
+  basePath: '/<your-repository-name>', // replace with your repository name
+  assetPrefix: '/<your-repository-name>', // needed for asset files like images
+  trailingSlash: true,
 };
 
 export default nextConfig;
