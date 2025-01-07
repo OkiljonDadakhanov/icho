9d6673a77fadb1148423657c6f3368234f04f59f
Akilhan <oqiljondadaxanov@gmail.com>
2025-01-07 06:42:34 +0500

    solved

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true, // Optional: Use SWC for minification
-<<<<<<< HEAD
-  basePath: '/icho',
-  assetPrefix: '/icho',
-=======
-  basePath: '/icho', // replace with your repository name
-  assetPrefix: '/icho', // needed for asset files like images
-  trailingSlash: true,
->>>>>>> ebdd9c734fc872418d326543daa212a37ed45b9f
+  basePath: "/icho",
+  assetPrefix: "/icho",
 };
 
 export default nextConfig;
